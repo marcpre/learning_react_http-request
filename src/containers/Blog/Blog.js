3e21488c34953baafc39444d64b6bd290ab1d374
marcpre <marcus.presich@gmail.com>
2018-02-26 05:26:53 +0000

    getting axios and adding posts

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -7,18 +7,34 @@ import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
 class Blog extends Component {
+    state = {
+        posts: []
+    }
+    
     componentDidMount() {
         axios.get('https://jsonplaceholder.typicode.com/posts')
-            .then
+            .then(response => {
+                const posts = response.data.slice(0, 4)
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                })
+                this.setState({posts: updatedPosts});
+                // console.log(response)
+            })
     }
     
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post key={post.id} title={post.title} author={post.author}/>
+        })
+        
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
                     <FullPost />
